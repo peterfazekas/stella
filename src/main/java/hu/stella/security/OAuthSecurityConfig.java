5a6f5dd863519478516aa83d50322e95c8dd40fc
peterfazekas <Peter_Fazekas@epam.com>
2017-07-03 06:41:36 +0200

    OAuth authentication

--- src/main/java/hu/stella/security/OAuthSecurityConfig.java
+++ src/main/java/hu/stella/security/OAuthSecurityConfig.java
@@ -1,15 +1,9 @@
 package hu.stella.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @EnableOAuth2Sso
 @Configuration
