a46c76e7131bbadcbe705878767b349d703c9b3c
Peter Fazekas <v-pfazekas@hotels.com>
2017-07-05 11:18:23 +0200

    Application config update

--- src/main/java/hu/stella/configuration/ApplicationConfig.java
+++ src/main/java/hu/stella/configuration/ApplicationConfig.java
@@ -1,10 +1,8 @@
 package hu.stella.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({InternationalizationConfig.class, OAuthSecurityConfig.class})
 public class ApplicationConfig {
 
 }
